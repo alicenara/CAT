c63140a89526b7315bbe504983a3f3f118805870
Alice <alicenara165@gmail.com>
2016-09-24 17:35:55 +0200

    First version of DNS poisoning

--- tests/libs/TestWebsite.py
+++ tests/libs/TestWebsite.py
@@ -28,6 +28,7 @@ class TestWebsite:
         self.website = None
         self.ip = None
         self.url = None
+        self.words = None
         self.original_ip = None
 
     def json_to_dict(self, json_string):
@@ -38,7 +39,8 @@ class TestWebsite:
             return None
         return new_dict
 
-    def basic_get_req(self, url):
+    def basic_get_req(self, url=None):
+        url = self.url if url is None else url
         try:
             self.website = requests.get(url, headers=self.headers, stream=True)
             self.ip = self.website.raw._connection.sock.getpeername()[0]
@@ -55,24 +57,38 @@ class TestWebsite:
         self.website = None
         return False
 
-    def check_ip(self, url, original_ip, neutral_dns_ip = "8.8.8.8"):
+    def check_ip(self, url=None, original_ip=None, neutral_dns_ip="8.8.8.8"):
+        url = self.url if url is None else url
+        original_ip = self.original_ip if original_ip is None else original_ip
+
         if self.ip == original_ip:
             self.log.info('IP is the same, DNS poisoning NOT detected (url: {})'.format(url))
             return True
         else:
             self.log.warning('Different IP detected: {}'.format(self.ip))
-            dns_search = sr1(IP(dst=neutral_dns_ip) / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qname=url)), verbose=0)
-            if dns_search[DNSRR].rrname == '.':
+            dns_search = None
+            try:
+                dns_search = sr1(IP(dst=neutral_dns_ip) / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qname=url)), verbose=0)
+            except OSError as oserr:
+                self.log.error('Error while trying to connect to DNS for URL {}: {}'. format(url, oserr))
+                return False
+
+            if dns_search is None or dns_search[DNSRR].rrname == '.':
                 self.log.warning('URL ({}) not found in neutral DNS.'.format(url))
+                return False
 
             obtained_ip = dns_search[DNSRR].rdata
             if original_ip == obtained_ip:
                 self.log.warning('Neutral DNS returns original IP - DNS poisoning detected for URL {}'.format(url))
             elif self.ip == obtained_ip:
                 self.log.warning('Neutral DNS returns given IP - maybe original IP not correct for URL {}'.format(url))
+                return True
+            else:
+                self.log.warning('Unexpectedly, given IP is totally different: URL {} - IP {}'.format(url, obtained_ip))
             return False
 
-    def search_keywords_content(self, words):
+    def search_keywords_content(self, words=None):
+        words = self.words if words is None else words
         failed_words = []
         for w in words:
             if w not in self.website.text:
@@ -91,7 +107,7 @@ class TestWebsite:
         :param web_params: a dict
         :return:
         """
-        necessary_params = ['url', 'words']
+        necessary_params = ['url', 'words', 'ip']
         not_found_params = []
 
         for w in necessary_params:
@@ -103,10 +119,13 @@ class TestWebsite:
                            "missing parameters = {}, received parameters = {}".format(not_found_params, web_params))
             return False
         else:
-            if self.basic_get_req(web_params['url']):
-                return self.search_keywords_content(web_params['words'])
+            self.url = web_params['url']
+            self.words = web_params['words']
+            self.original_ip = web_params['ip']
+            if self.basic_get_req():
+                return self.search_keywords_content()
             else:
-                return False
+                return self.check_ip()
 
 
 
